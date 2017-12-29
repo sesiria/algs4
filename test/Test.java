c3646817b1ff183eb16bb36ce10c8856d4645478
Lintao Deng <stjohnson_free@hotmail.com>
2017-12-29 16:14:25 +0800

    Initial commit

---
+++ test/Test.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.StdOut;
+
+class Test {
+    static final String STR = "test string";
+
+    public static void main(String []args) {
+        int i = 0;
+        int b = i++;
+        test();
+        System.out.println("Hello world!\n");
+        StdOut.println("");
+        return;
+    }
+
+    public static void test() {
+        int i = 1;
+        int b = i++;
+
+    }
+}
